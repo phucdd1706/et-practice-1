bdd13b09fbbfc0f0a5b767f0f2d429900ca7b0eb
phucnd-137 <dinhphuc.work@gmail.com>
2023-03-10 22:19:12 +0700

    change layout initial

--- src/layout/ContactPage/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ContactPage = () => {
-  return <div>Hello World</div>;
-};
-
-export default ContactPage;
--- src/layout/ContentPage/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ContentPage = () => {
-  return <div>Hello World</div>;
-};
-
-export default ContentPage;
