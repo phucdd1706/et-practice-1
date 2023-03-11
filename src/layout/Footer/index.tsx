dec11b409fe538241c13d39989005aae4af93e20
phucnd-137 <dinhphuc.work@gmail.com>
2023-03-11 15:02:53 +0700

    update

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import EmailImage from "~/assets/img/email.png";
 
 // Footer with background image and div with text
 const Footer = () => {
@@ -6,8 +7,26 @@ const Footer = () => {
     <div className="footer-container">
       <div className="footer-img">
         <div className="footer-content">
-          <div>231312</div>
-          <div>loass</div>
+          <div className="text-center">
+            <img
+              src={EmailImage}
+              style={{ color: "white" }}
+              alt="logo"
+              width={50}
+              height={50}
+            />
+            <p className="text-4xl margin-none">Lorem!</p>
+          </div>
+          <div className="footer-email">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="input-email"
+              placeholder="Enter Emails Address ..."
+            />
+            <button className="button-notify">Notify me!</button>
+          </div>
         </div>
       </div>
       {/* black div */}
