bbcdbdaa67bc22029a5eecc9a39b379f1462dcdc
phucnd-137 <dinhphuc.work@gmail.com>
2023-03-12 06:13:29 +0700

    update

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -18,20 +18,24 @@ const Footer = () => {
             <p className="text-4xl margin-none">Lorem!</p>
           </div>
           <div className="footer-email">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="input-email"
-              placeholder="Enter Emails Address ..."
-            />
-            <button className="button-notify">Notify me!</button>
+            <div>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="input-email"
+                placeholder="Enter Emails Address ..."
+              />
+            </div>
+            <div>
+              <button className="button-notify">Notify me!</button>
+            </div>
           </div>
         </div>
       </div>
       {/* black div */}
       <div className="footer-div">
-        <p className="text-2xl">Copyright © Your Website 2022</p>
+        <p>Copyright © Your Website 2022</p>
       </div>
     </div>
   );
--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,19 +1,28 @@
 import React from "react";
+import MenuIcon from "~/assets/img/menu-icon.svg";
 
 const Header = () => {
   return (
     <div className="header-container">
-      <h1 className="text-2xl logo">Entry Test</h1>
+      <a href="/" className="text-2xl logo">
+        Entry Test
+      </a>
       <div className="menu">
-        <a href="/" className="menu-item text-lg">
+        <a href="#About" className="menu-item text-lg">
           About
         </a>
-        <a href="/" className="menu-item text-lg">
+        <a href="#Projects" className="menu-item text-lg">
           Projects
         </a>
-        <a href="/" className="menu-item text-lg">
+        <a href="#Contact" className="menu-item text-lg">
           Contact
         </a>
+        <button className="btn-menu" type="button">
+          <div className="btn-container">
+            <span className="text-lg">Menu</span>
+            <img className="menu-icon" src={MenuIcon} alt="menu icon" />
+          </div>
+        </button>
       </div>
     </div>
   );
